8f12019bd476aa6f68042b274fb89911b1fad43d
kacperluk <kacper.luk@o2.pl>
2020-07-16 11:42:49 +0200

    Section 7 Web Servers (Weather app)

---
+++ web-server/src/app.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlerbars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Kacper'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Weather App',
+        name: 'Kacper'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Weather App',
+        message: 'some message',
+        name: 'Kacper'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        temperature: 31,
+        location: 'Wroclaw'
+    })
+})
+
+app.get('help/*', (req, res) => {
+    res.send('Article not found')
+})
+
+app.get('*', (req, res) => {
+    res.send('404 not found')
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
