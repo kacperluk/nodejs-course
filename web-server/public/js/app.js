47ec8c6b78229c144b8c6d41f9b2f72d7e8912bc
kacperluk <kacper.luk@o2.pl>
2020-07-20 14:49:24 +0200

    Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,7 @@
 console.log('Client side javascript file is loaded!')
+
+fetch('http://localhost:3000/weather?address=wroclaw').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
