4cf5329b7f4ca2c1761a57bee539132e8661d1c9
kacperluk <kacper.luk@o2.pl>
2020-07-20 15:36:39 +0200

    Creating Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,7 +1,18 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=wroclaw').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
+
+
+const weatherForm = document.querySelector('form')
+const searchElemet = document.querySelector('input')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = searchElemet.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            console.log(data)
+        })
     })
 })
